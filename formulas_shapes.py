95d3e57b42ece1153d43d9e0dc52b101ae7012af
hi-lary <113123171+hi-lary@users.noreply.github.com>
2023-02-10 13:48:37 -0600

    Update formulas_shapes.py

---
+++ formulas_shapes.py
@@ -0,0 +1,19 @@
+# program text
+
+from math import pi
+
+h = 5.0  # height
+b = 2.0  # base
+r = 1.5  # radius
+
+area_parallelogram = h * b
+print(f'The area of the parallelogram is {area_parallelogram}')
+
+area_square = b ** 2
+print(f'The area of the square is {area_square}')
+
+area_circle = pi * r ** 2
+print(f'The area of the circle is {area_circle}')
+
+volume_cone = 1.0 / 3 * pi * r ** 2 * h
+print(f'The volume of the cone is {volume_cone}')
