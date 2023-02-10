921979d3f1ce72389288385c8fe7023cdd8572ec
hi-lary <113123171+hi-lary@users.noreply.github.com>
2023-02-10 16:48:22 -0600

    Update egg.py

--- egg.py
+++ egg.py
@@ -9,6 +9,6 @@ Tw = 100  # temp of water Celsius
 Ti = eval(input('initial temperature of egg in celsius:'))  # initial temp of egg celsius
 Ty = 70  # cooked yolk temp Celsius
 
-time = (m**(2/3)*c*p**(1/3))/(k*pi**2*((4*pi)/3)**(2/3))*log(0.76*((Ti-Tw)/(Ty-Tw)))  #seconds
+time = (m**(2/3)*c*p**(1/3))/(k*pi**2*((4*pi)/3)**(2/3))*log(0.76*((Ti-Tw)/(Ty-Tw)))  # seconds
 
 print(f'Given a {m}g egg at an initial temperature of {Ti}C, it will take {time} seconds to cook the yolk.')
